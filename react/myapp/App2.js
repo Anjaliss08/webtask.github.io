8fc5c5a6ac5dd741ac97eabf6b978c0512f014b9
Anjaliss08 <anjaliweb08@gmail.com>
2022-08-27 20:18:52 +0530

    webtask

---
+++ react/myapp/App2.js
@@ -0,0 +1,18 @@
+import Header from './component/header';
+import Footer from './component/footer ';
+import Home from './component/home';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Home/>
+      <Footer/>
+
+    </div>
+  );
+}
+
+export default App;
